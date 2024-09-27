1b76320c420c2865c842b2a12b0e66d296831e75
Akachi <trusteddev1325@gmail.com>
2024-09-27 11:12:05 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { Bot } from "grammy";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+console.log(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN); // Your bot token
+
+app.post("/generate-invoice", async (req, res) => {
+  const title = "Test Product";
+  const description = "Test description";
+  const payload = "{}";
+  const currency = "XTR";
+  const prices = [{ amount: 1, label: "Test Product" }];
+
+  const invoiceLink = await bot.api.createInvoiceLink(
+    title,
+    description,
+    payload,
+    "", // Provider token must be empty for Telegram Stars
+    currency,
+    prices,
+  );
+
+  res.json({ invoiceLink });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
